472349ea7de32b185c2fea5d68beb31ed7fec2b1
longqi_cm@deepin.com <longqi_cm@deepin.com>
2020-01-12 15:36:20 +0800

    feat: add main.go sort.go hannuo.go

---
+++ main/hannuo.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+)
+
+func moveHannuo(count byte){
+	moveAction("A", "B", "C", count);
+}
+
+func moveAction(z1, z2, zb string, count byte){
+	if count == 1{
+		fmt.Printf("move Top from %s to %s.\n", z1, z2);
+		return;
+	}
+	moveAction(z1, zb, z2, count - 1);
+	moveAction(z1, z2, zb, 1);
+	moveAction(zb, z2, z1, count - 1);
+}
---
+++ main/sort.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+)
+
+func sort(arr []int){
+	fmt.Println("before sort:%a", arr)
+	for i:=1; i < len(arr); i++ {
+		var ta = arr[0:i+1];
+		for j,_ := range ta{
+			if ta[j] <= arr[i] {
+				var tv = arr[i]
+				copy(arr[j+1:i+1], arr[j:i])
+				arr[j]=tv
+				break
+			}
+		}
+	}
+	fmt.Println("end sort:%a", arr)
+}
+
