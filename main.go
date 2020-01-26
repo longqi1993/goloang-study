e1c299c60ab976ad0846e24a064e803406aff436
longqi <longqi_cm@deepin.com>
2020-01-26 14:38:11 +0800

    feat: test load gsetting xml file name to tree

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import(
-	 _ "fmt"
-	 "gutils"
+	_ "fmt"
+	"io/ioutil"
+	"strings"
+
+	"gutils"
 )
 
+const (
+	gsPath = "/usr/share/glib-2.0/schemas/"
+)
+
+
 func main() {
-	tt := gutils.NewTree(".")
+	gt := gutils.NewTree(" ")
+	infos, _ := ioutil.ReadDir(gsPath)
+	for _, v := range infos {
+		addGsPath(v.Name(), gt)
+	}
 
-	tn := tt.Root()
-	tn.AddChild("t1")
-	tn2 := tn.AddChild("t2")
-	ttn1 := tn2.AddChild("ts1")
-	ttn1.AddChild("tts1")
-	ttn1.AddChild("tts2")
-	ttn1.AddChild("tts3")
-	tn2.AddChild("ts2")
+	gt.PrintTree()
+}
 
-	tt.PrintTree()
+func addGsPath(p string, t *gutils.Tree) {
+	ss := strings.Split(p, ".")
+	tn := t.Root()
+	for idx := 0; idx < len(ss) - 2; idx++ {
+		tn = tn.AddChild(ss[idx])
+	}
 }
